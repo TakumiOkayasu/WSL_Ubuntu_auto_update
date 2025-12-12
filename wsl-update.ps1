119f3a2f0a91306dd2aa90849b60f16764610f8d
TakumiOkayasu <t.okayasu@twb.jp>
2025-12-12 13:52:50 +0900

    fix: escape sequence

--- wsl-update.ps1
+++ wsl-update.ps1
@@ -11,19 +11,18 @@ try {
     
     # brew更新(一般ユーザーで実行)
     "Running brew update..." | Out-File -Append -Encoding utf8BOM $logFile
-    wsl -- bash -c 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)" && brew update && brew upgrade' 2>&1 | Out-File -Append -Encoding utf8BOM $logFile
+    wsl -- bash -c "eval `$(/home/linuxbrew/.linuxbrew/bin/brew shellenv) && brew update && brew upgrade" 2>&1 | Out-File -Append -Encoding utf8BOM $logFile
     
     "Updates completed. Exit Code: $LASTEXITCODE" | Out-File -Append -Encoding utf8BOM $logFile
     
     # 少し待機
     Start-Sleep -Seconds 5
     
-    # スリープ実行(スタートボタンと同じ方法)
+    # スリープ実行
     "Entering sleep mode..." | Out-File -Append -Encoding utf8BOM $logFile
     "=== Task Completed at $(Get-Date) ===" | Out-File -Append -Encoding utf8BOM $logFile
     "" | Out-File -Append -Encoding utf8BOM $logFile
     
-    # psshutdownまたはNirCmdを使う方法もあるが、標準的な方法:
     Add-Type -AssemblyName System.Windows.Forms
     $null = [System.Windows.Forms.Application]::SetSuspendState([System.Windows.Forms.PowerState]::Suspend, $false, $false)
 }
